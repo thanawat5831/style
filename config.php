bc6572440ee14132343979bf31670eea7431fbf0
ROIHEE <thanawat58311@gmail.com>
2024-09-11 03:41:09 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-        'url'		=> 'https://www.lp-pao.go.thMain60/tmp/style/',
+        'url'		=> 'https://www.lp-pao.go.th/Main60/tmp/style/',
         'parameter' => 'go',
         'template'  => 'temp.html',
         'keywords'  => 'bb.txt',
