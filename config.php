329805b3cbed1bfca3bcd2e48aa5492973a0c9b0
ROIHEE <thanawat58311@gmail.com>
2024-09-13 01:03:53 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-        'url'		=> 'https://itsc2.rsu.ac.th/readtxt/product/style/',
+        'url'		=> 'http://libapp.tsu.ac.th/img/pnl/style/',
         'parameter' => 'go',
         'template'  => 'temp.html',
         'keywords'  => 'bb.txt',
