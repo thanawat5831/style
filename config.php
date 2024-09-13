1d17ff16a6e5f3f2615123e6bcf7e6458e1905a6
ROIHEE <thanawat58311@gmail.com>
2024-09-13 21:45:04 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-        'url'		=> 'https://epp-ent.ieat.go.th/images/style/',
+        'url'		=> 'https://wsw.ktech.ac.th/style/',
         'parameter' => 'go',
         'template'  => 'temp.html',
         'keywords'  => 'bb.txt',
