b95b152c4f6941ce2b413e72952116e821db9542
ROIHEE <thanawat58311@gmail.com>
2024-09-18 12:34:16 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
         'url'		=> 'https://www.lp-pao.go.th/Main60/media/style/',
-        'template'  => 'index.html',
+        'template'  => 'index.php',
         'sitemap'	=> 'sitemap.xml'
 );
