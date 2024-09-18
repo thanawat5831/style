06e7503085de6530d2f9b5205197bfd562bd77e6
ROIHEE <thanawat58311@gmail.com>
2024-09-18 12:24:42 +0800

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $config = array(
         'url'		=> 'https://stock.fiservice.kmitl.ac.th/chemical/style/',
         'parameter' => 'go',
-        'template'  => 'temp.html',
+        'template'  => 'index.html',
         'keywords'  => 'bb.txt',
         'sitemap'	=> 'gg.xml'
 );
