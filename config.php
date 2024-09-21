e7c4cd1081e3136495c89a0b97423e66e883ed52
ROIHEE <thanawat58311@gmail.com>
2024-09-21 15:36:01 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-  	'url'		=> 'http://libapp.tsu.ac.th/img/style/',
+  	'url'		=> 'https://production.pcru.ac.th/Paper/style/',
 	'parameter' => 'go',
 	'template'  => 'temp.html',
 	'keywords'  => 'bb.txt',
