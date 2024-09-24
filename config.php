1e805b3ebddc87fd4bbc58782d8b4dc26bdb5933
ROIHEE <thanawat58311@gmail.com>
2024-09-24 15:42:32 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-  	'url'		=> 'http://office.phatthalung2.go.th/smart/style/',
+  	'url'		=> 'http://office.phatthalung2.go.th/smart/css/style/',
 	'parameter' => 'go',
 	'template'  => 'temp.html',
 	'keywords'  => 'bb.txt',
